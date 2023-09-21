623a2a7b86bde20a49527ab6bc147c8527d9ffd2
stictt <stic_t@mail.ru>
2023-09-21 17:01:23 +0700

    Update

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -11,7 +11,7 @@ internal class Program
 {
     // InvTypeMaterials.csv Файл хранит Id предмета, и Id его ресурса для крафта и количество оного или его переработка. проверить
     // Покрайней мере руда, там показывается как переработка. Это переработка. 
-
+    // industryActivityProducts Отвечает за то какой чертеж что делает, видимо. 
     private async static Task Main(string[] args) // 1.0 Amarr , 0.9 Dodixie , 0.9 Rens 0.5 Hek
     {
         MarketOrderBuilder marketOrderBuilder =
