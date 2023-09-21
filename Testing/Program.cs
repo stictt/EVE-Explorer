0f885abd1a9435448b4bc537e8e9bbe7e294c153
stictt <stic_t@mail.ru>
2023-09-21 14:39:45 +0700

    update

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -9,7 +9,9 @@ using System.Threading.RateLimiting;
 
 internal class Program
 {
-    // InvTypeMaterials.csv Файл хранит Id предмета, и Id его ресурса для крафта и количество оного
+    // InvTypeMaterials.csv Файл хранит Id предмета, и Id его ресурса для крафта и количество оного или его переработка. проверить
+    // Покрайней мере руда, там показывается как переработка. Это переработка. 
+
     private async static Task Main(string[] args) // 1.0 Amarr , 0.9 Dodixie , 0.9 Rens 0.5 Hek
     {
         MarketOrderBuilder marketOrderBuilder =
